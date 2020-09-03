367a2933095ae8bbad0ea1f8eb5202ff86ac07ae
Buddy Agyin <agyin3@gmail.com>
2020-09-03 17:03:23 -0600

    add code for challenge

---
+++ Character/character.js
@@ -0,0 +1,26 @@
+// Make an axios GET request to the following address: https://rickandmortyapi.com/api/character/2
+// Study the response data you get back, closely.
+
+// Create a function that takes a single character object and returns the following markup.
+
+/* 
+        <div class='card'>
+            <h2>{name of character}</h2>
+            <img src={image of character} />
+            <div class="grid">
+                <div>
+                    <p>Status: {status of character}</p>
+                    <p>Species: {species of character} </p>
+                </div>
+                <div>
+                    <p>Gender: {gender of character} </p>
+                    <p>Location: {location of character}</p>
+                </div>
+        </div>
+*/
+
+// Use your function to create a card for the character, and append the card to div.container in the DOM.
+
+function cardMaker(){
+    // your code here
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # applied-js-sprint-review
+
+## Project Setup
+- Navigate to the root of the project with your command line.
+- Run `npm install` to download any dependencies listed in the `package.json` file.
+- Run `npm start` to compile your project and launch a development server.
+
+## Project Description
+
+1. Setup axios (either with CDN or npm)
+2. Navigate to `character.js` and complete the task
